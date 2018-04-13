3bf68227d1367218999834da43466cb5506afe0b
Daniel Matthes <dmatthes@cbs.mpg.de>
2018-04-13 10:05:23 +0200

    Module coSMIC_MotionDetetction –  import of module parallel → the parport is opened and connected with the data processing instance of both sensors → boolean var useTwoClients add → thereby the second sensor can be easily switched off for debugging purpose Module coSMIC_SensDataProc – import of module parallel → new input parameter to class instance object added: par → with this a parallel port will be conencted with the instance → the dataProcessing function now transmit via parport 0x64 when a predefine threshold is exceeded and 0x65 when the motion signal fall below this threshold File .gitignore – entry .metawaer added

--- coSMIC_MotionDetection.py
+++ coSMIC_MotionDetection.py
@@ -12,6 +12,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 from coSMIC_SensDataProc import SensDataProc
+import parallel
 import time
 
 from pymetawear.client import MetaWearClient
@@ -19,25 +20,30 @@ from pymetawear.client import MetaWearClient
 address1 = 'E3:53:A4:26:93:0F'
 address2 = 'F4:94:79:03:D2:93'
 
+useTwoClients = False
+
 client1 = MetaWearClient(str(address1), debug=False)
 print("New client created: {0}".format(client1))
 
-client2 = MetaWearClient(str(address2), debug=False)
-print("New client created: {0} \n".format(client2))
+if useTwoClients:
+	client2 = MetaWearClient(str(address2), debug=False)
+	print("New client created: {0} \n".format(client2))
 
 print("Get possible accelerometer settings of client 1...")
 settings = client1.accelerometer.get_possible_settings()
 print(settings)
 
-print("Get possible accelerometer settings of client 2...")
-settings = client2.accelerometer.get_possible_settings()
-print(settings)
+if useTwoClients:
+	print("Get possible accelerometer settings of client 2...")
+	settings = client2.accelerometer.get_possible_settings()
+	print(settings)
 
 time.sleep(1.0)
 
 print("\nWrite accelerometer settings...")
-client1.accelerometer.set_settings(data_rate=3.125, data_range=4.0)
-client2.accelerometer.set_settings(data_rate=3.125, data_range=4.0)
+client1.accelerometer.set_settings(data_rate=200, data_range=4.0)
+if useTwoClients:
+	client2.accelerometer.set_settings(data_rate=200, data_range=4.0)
 
 time.sleep(1.0)
 
@@ -45,18 +51,24 @@ print("\nCheck accelerometer settings of client 1...")
 settings = client1.accelerometer.get_current_settings()
 print(settings)
 
-print("Check accelerometer settings of client 2...")
-settings = client2.accelerometer.get_current_settings()
-print(settings)
+if useTwoClients:
+	print("Check accelerometer settings of client 2...")
+	settings = client2.accelerometer.get_current_settings()
+	print(settings)
 
-sens1 = SensDataProc()
-sens2 = SensDataProc()
+parPort = parallel.Parallel()
+sens1 = SensDataProc(parPort)
+if useTwoClients:
+	sens2 = SensDataProc(parPort)
 
 print("\nSubscribing to accelerometer signal notifications...")
-client1.accelerometer.high_frequency_stream = False
-client2.accelerometer.high_frequency_stream = False
+client1.accelerometer.high_frequency_stream = True
+if useTwoClients:
+	client2.accelerometer.high_frequency_stream = True
+
 client1.accelerometer.notifications(lambda data : sens1.dataProcessing(data, 1))
-client2.accelerometer.notifications(lambda data : sens2.dataProcessing(data, 2))
+if useTwoClients:
+	client2.accelerometer.notifications(lambda data : sens2.dataProcessing(data, 2))
 
 prompt = "\nPress q and Enter to quit the programm...\n\n" 
 message = "" 
@@ -65,10 +77,12 @@ while message != 'q':
 
 print("Unsubscribe to notification...")
 client1.accelerometer.notifications(None)
-client2.accelerometer.notifications(None)
+if useTwoClients:
+	client2.accelerometer.notifications(None)
 
 time.sleep(5.0)
 
 client1.disconnect()
-client2.disconnect()
+if useTwoClients:
+	client2.disconnect()
 
--- coSMIC_SensDataProc.py
+++ coSMIC_SensDataProc.py
@@ -7,17 +7,28 @@ Created by dmatthes1982 <dmatthes@cbs.mpg.de>
 Created on 2018-04-11
 """
 
+import parallel
 import numpy as np
 from numpy_ringbuffer import RingBuffer
 import pdb
 
-class SensDataProc: 
-	def __init__(self):
+class SensDataProc:
+	def __init__(self, par):
 		self.ringBuffer = RingBuffer(capacity=10, dtype=np.float)
+		self.lpt = par
+		self.prevMeanAccel = 1;
 
 	def dataProcessing(self, data, num):
 		epoch = data['epoch']	
 		accel = data['value']
 		self.ringBuffer.append(abs(accel.x) + abs(accel.y) + abs(accel.z))
 		meanAccel = np.mean(np.array(self.ringBuffer))
-		print("[%i, %i] - %f" % (num, epoch, meanAccel))
+		#print("[%i, %i] - %0.2f" % (num, epoch, meanAccel))
+		if meanAccel > 2 and self.prevMeanAccel < 2:
+			print("[%i, %i] - %x" % (num, epoch, 0x64))
+			self.lpt.setData(0x64)
+		if meanAccel < 2 and self.prevMeanAccel > 2:
+			print("[%i, %i] - %x" % (num, epoch, 0x65))
+			self.lpt.setData(0x65)
+			
+		self.prevMeanAccel = meanAccel	
